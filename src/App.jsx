5c67e8653d716867dbcca870f3cc275415ed496d
NiteshGuptaa <nkgupta62093@gmail.com>
2025-09-11 22:47:31 +0530

    add FE

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-amber-800 bg-amber-400 text-4xl font-bold">hello!</h1>
+      <h2>ncidhks</h2>
+    </>
+  )
+}
+
+export default App
